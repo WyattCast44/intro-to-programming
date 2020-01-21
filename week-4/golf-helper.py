3535d2736cfec51aef3ed875b45ebb7b0567867e
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2020-01-21 10:37:05 -0800

    wip

---
+++ week-4/golf-helper.py
@@ -0,0 +1,39 @@
+# Header
+
+"""
+Week 4 Review - Part 1
+Wyatt Castaneda
+SWDV 600: INTRO TO PROGRAMMING
+"""
+
+# Problem 
+
+"""
+You have been contracted by a golf training company in need of a new golf club recommendation app for their clients.
+
+Your new client is looking for an application where a user enters some information about their current golfing situation and the app replies with what club the person should use. They want to start simple first. The application should ask the user, in this order, if
+
+they have hit the ball on the green (answer should be `y` or `n`)
+how far they are from the hole 
+If the ball is on the green, then the app should recommend using the 'Putter' club. Otherwise, if the distance is 200 yards or more, it should recommend the 'Driver', between 140 and 200 yards it should recommend the '5-Iron', between 100 and 140 yards the '9-Iron', and for any other distance it should recommend the 'Pitching Wedge'.
+
+You are to write a program that makes those recommendations based on the user's input. You can assume the user will only enter valid values. Here are two examples:
+
+Welcome to the Golf Club Helper!
+Tell me your situation, and I'll recommend a club
+
+Did you hit it on the green (y/n)? y
+How far is the ball from the hole? 15
+
+I recommend using your Putter
+
+Welcome to the Golf Club Helper!
+Tell me your situation, and I'll recommend a club
+
+Did you hit it on the green (y/n)? n
+How far is the ball from the hole? 110
+
+I recommend using your 9-Iron
+"""
+
+# Solution
---
+++ week-4/sales-invoice-finder.py
@@ -0,0 +1,51 @@
+# Header
+
+"""
+Week 4 Review - Part 2
+Wyatt Castaneda
+SWDV 600: INTRO TO PROGRAMMING
+"""
+
+# Problem 
+
+"""
+ou are working for a computer parts manufacturer that needs a new program to find sales information based on one of two pieces of information
+
+- an invoice identifier (id) or
+- a customer's last name (lname)
+
+Your company logs each parts sale in an Excel file saved as a CSV file called `sales_data.csv` (NOTE: if you think no company would ever do this, you are incorrect: it has been done). The first line in that file contains the column headers: textual descriptions of what data is in each column. The columns are invoice id, a customer's first name, last name, part number, quantity, and total. 
+
+Your goal is to make this file searchable. Your program should prompt the user for the following inputs, in this order:
+
+- Whether they want to search using an invoice id (input of `id`) or by a customer's last name (input of `lname`). The program should reject any input that is not `id` or `lname`, forcing the user to choose one of those two options.
+- The search term, either an `id` value or an `lname` value
+
+After the user enters these inputs, the program should open the data file (note that the user does not input the data file), then search within the chosen column (i.e., either `id` or `lname`, but not both) for the input value. If the program finds a match in any invoice recrords, then it it should print (not save) all recorded invoices that match. Finally it should print the total number of records found that match the search term. 
+
+Here are three examples:
+
+Search by invoice id (id) or customer last name (lname)? firstname
+ERROR: You must enter either 'id' for invoice id search or 'lname' for customer last name search
+Search by invoice id (id) or customer last name (lname)? lname
+Enter your search term: Hutz
+87681,Lionel,Hutz,218,1,50.83
+34018,Lionel,Hutz,112,3,88.88
+34018,Lionel,Hutz,386,3,86.04
+34018,Lionel,Hutz,216,1,53.54
+4 records found.
+
+Search by invoice id (id) or customer last name (lname)? id
+Enter your search term: 93303
+93303,Frank,Grimes,392,2,90.74
+93303,Frank,Grimes,142,3,73.2
+93303,Frank,Grimes,353,1,45.87
+3 records found.
+
+Search by invoice id (id) or customer last name (lname)? lname
+Enter your search term: Maryville
+0 records found.
+
+"""
+
+# Solution
