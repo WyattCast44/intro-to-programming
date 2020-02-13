eab92746cd8e24f6bd419ab8bb5fc0ed1df5341f
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2020-02-12 21:48:31 -0800

    wip

--- final-project/options/help.py
+++ final-project/options/help.py
@@ -1,14 +1,14 @@
+from console import Version
+
 class Help:
     
     signature = '--h'
 
     description = 'Display the application help screen'
 
-    def help(self):
-        print(self)
-        
     def __init__(self, application):
         self.application = application
 
     def handle(self):
+        Version(self.application).handle()
         self.application.console().line(f'\nRunning the help command')
